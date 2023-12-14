c6863e44b138657d76da9c335e2b4b76a38f3b52
전승범 <seungbeomjeon@pidata.co.kr>
2023-12-14 12:58:25 +0900

    홈 UI 추가

---
+++ app/components/products/ProductCard.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { formatPrice } from '@/utils/formatPrice';
+import { truncateText } from '@/utils/truncateText';
+import { Rating } from '@mui/material';
+import Image from 'next/image';
+
+interface ProductCardProps {
+    data: any;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+    const productRating = data.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) / data.reviews.length;
+
+    return (
+        <div className="col-span-1 cursor-pointer border-[1.2px] border-t-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm">
+            <div className="flex flex-col items-center w-full gap-1">
+                <div className="relative w-full overflow-hidden aspect-square">
+                    <Image fill src={data.images[0].image} alt={data.name} className="object-contain w-full h-full" />
+                </div>
+                <div className="mt-4">{truncateText(data.name)}</div>
+                <div>
+                    <Rating value={productRating} readOnly></Rating>
+                </div>
+                <div>{data.reviews.length} reviews</div>
+                <div className="font-semibold">{formatPrice(data.price)}</div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
 import Image from 'next/image';
+import Container from './components/Container';
+import HomeBanner from './components/HomeBanner';
+import { products } from '@/utils/products';
+import { truncateText } from '@/utils/truncateText';
+import ProductCard from './components/products/ProductCard';
 
 export default function Home() {
-    return <div>e-commerce</div>;
+    return (
+        <div className="p-8">
+            <Container>
+                <div>
+                    <HomeBanner />
+                </div>
+                <div className="grid grid-cols-2 gap-8 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+                    {products.map((product: any) => {
+                        return <ProductCard key={crypto.randomUUID()} data={product} />;
+                    })}
+                </div>
+            </Container>
+        </div>
+    );
 }
